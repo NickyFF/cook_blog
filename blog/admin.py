b63aaf01648927fe0949d3b1dde27d812d4fe7e1
NickyFF <nikirakryzx@gmail.com>
2023-06-16 14:09:54 +0300

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+
+from . import models
+
+# Добавление рецепта в интерфейсе поста
+class RecipeInline(admin.StackedInline):
+    model = models.Recipe
+    extra = 1
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "category", "create_at"]
+    inlines = [RecipeInline]
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name", "prep_time", "cook_time", "post"]
+
+
+
+admin.site.register(models.Category, MPTTModelAdmin)
+admin.site.register(models.Tag)
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Comment)
