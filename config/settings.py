12a73461e1676f7631933f6603d96eb700bb4637
NickyFF <nikirakryzx@gmail.com>
2023-06-21 21:24:44 +0300

    gallery update

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'ckeditor',
 
     'blog',
-    'contact'
+    'contact',
+    'gallery',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache'),
+    }
+}
